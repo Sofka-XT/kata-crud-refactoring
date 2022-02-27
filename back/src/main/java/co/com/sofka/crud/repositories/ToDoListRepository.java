0a15c97a422bbe280126c03260d95ccec11ada62
Santiago Barrera <santiago.barrerae@gmail.com>
2022-02-26 23:41:54 -0500

    Se crea ToDoList repository

---
+++ back/src/main/java/co/com/sofka/crud/repositories/ToDoListRepository.java
@@ -0,0 +1,8 @@
+package co.com.sofka.crud.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ToDoListRepository extends CrudRepository {
+}
