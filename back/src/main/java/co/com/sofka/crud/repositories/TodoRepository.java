e9769994172e7d1f0e0e901a47f24467d5ec5da2
Santiago Barrera <santiago.barrerae@gmail.com>
2022-02-26 23:42:24 -0500

    Se agrega etiqueta repository

--- back/src/main/java/co/com/sofka/crud/repositories/TodoRepository.java
+++ back/src/main/java/co/com/sofka/crud/repositories/TodoRepository.java
@@ -2,6 +2,8 @@ package co.com.sofka.crud.repositories;
 
 import co.com.sofka.crud.models.Todo;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TodoRepository extends CrudRepository<Todo, Long> {
 }
