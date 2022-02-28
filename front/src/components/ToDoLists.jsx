8d3c770b50d2baeddf7a20f5bb5c96a2f7b274bd
Santiago Barrera <santiago.barrerae@gmail.com>
2022-02-27 23:58:58 -0500

    se hacen peticiones con entity

--- front/src/components/ToDoLists.jsx
+++ front/src/components/ToDoLists.jsx
@@ -1,51 +1,53 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { fetchListas, fetchApi, fetchDTO } from "./GetFunctions";
 import { ToDosItems } from "./ToDosItems";
 
 export const ToDoLists = (props) => {
   const listas = props.Lists ? props.Lists : []
-  const dtos = props.DTOS ? props.DTOS : []
+  const dtos = props.DTOS ? props.DTOS : [];
+  const [Estado, setEstado] = useState(0);
   useEffect(() => {
     fetchListas()
     .then((response) => response.json())
-    .then((list) => {
-      let arrayDTOS = []
-      list.forEach(element => {
-            console.log(element.id)
-        fetchDTO(element.id)
-          .then((response) => response.json())
-          .then((dto) => {
-            arrayDTOS.push(dto)
-          });
-      });
-      props.setDTOS(arrayDTOS);
-    })
+    .then((list) => props.setLists(list))
+    // .then((list) => {
+    //   let arrayDTOS = []
+    //   list.forEach(element => {
+    //         // console.log(element.id)
+    //     fetchDTO(element.id)
+    //       .then((response) => response.json())
+    //       .then((dto) => {
+    //         arrayDTOS.push(dto)
+    //       })
+    //   })
+    //   dtos.push(arrayDTOS)
+    //   // props.setDTOS(arrayDTOS)
+    // });
 
   }, [])
 
-  console.log(dtos);
   if (listas) {
     return (
       <div>
-        {listas.map((lista) => {
+        {listas.map(dto => {
           return (
             <div
-              key={lista.id}
+              key={dto.id}
               className="border border-3 p-3 mb-2 bg-light text-dark"
             >
               <div className="d-flex flex-row bd-highlight mb-3 align-items-center">
-                <h1>{lista.name}</h1>
+                <h1>{dto.name}</h1>
                 <button
                   className="btn btn-danger m-3"
                   onClick={() => {
-                    props.eliminarLista(lista);
+                    props.eliminarLista(dto);
                   }}
                 >
                   Eliminar
                 </button>
               </div>
-              {props.insertForm(lista)}
-              {props.insertTodo(lista)}
+              {props.insertForm(dto)}
+              {props.insertTodo(dto)}
             </div>
           );
         })}
