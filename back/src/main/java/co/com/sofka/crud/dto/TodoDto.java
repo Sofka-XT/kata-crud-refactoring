c94541fc976b546cf8be66d97951baea5af0b050
riveco0120 <richardsk.vellojin@gmail.com>
2022-05-04 16:49:23 -0500

    Se actualizo tododto

--- back/src/main/java/co/com/sofka/crud/dto/TodoDto.java
+++ back/src/main/java/co/com/sofka/crud/dto/TodoDto.java
@@ -4,18 +4,26 @@ package co.com.sofka.crud.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Setter
 @Getter
 public class TodoDto {
 
     private Long id;
+
     private String name;
     private boolean completed;
-    private Long groupListId;
+    private String groupListId;
 
     public TodoDto(){
 
     }
 
+    public TodoDto(String name, boolean completed, String groupListId) {
+        this.name = name;
+        this.completed = completed;
+        this.groupListId = groupListId;
+    }
 }
 
