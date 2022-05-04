9376099683dd68e31258a9246fab1ade8060449e
riveco0120 <richardsk.vellojin@gmail.com>
2022-05-04 16:45:42 -0500

    Se creo clase para mappear
    Clase para mappear las entidades en dto

--- back/src/main/java/co/com/sofka/crud/dto/TodoListDto.java
+++ back/src/main/java/co/com/sofka/crud/dto/TodoListDto.java
@@ -1,2 +1,18 @@
-package co.com.sofka.crud.dto;public class TodoListDto {
+package co.com.sofka.crud.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class TodoListDto {
+    public Long id;
+    public String nameList;
+    private List<TodoDto> todos = new ArrayList<>();
+
+
+
 }
