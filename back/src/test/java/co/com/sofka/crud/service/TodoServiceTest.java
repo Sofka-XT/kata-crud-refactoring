ab95f0e55a6b4a16962836760d86589d94329efa
riveco0120 <richardsk.vellojin@gmail.com>
2022-05-04 15:01:21 -0500

    Agregando test
    Se agregan los test para listar por id, listar todos y eliminar

--- back/src/test/java/co/com/sofka/crud/service/TodoServiceTest.java
+++ back/src/test/java/co/com/sofka/crud/service/TodoServiceTest.java
@@ -1,16 +1,19 @@
 package co.com.sofka.crud.service;
 
-import co.com.sofka.crud.dto.TodoDto;
 import co.com.sofka.crud.models.Todo;
 import co.com.sofka.crud.repository.TodoRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.stereotype.Repository;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @DataJpaTest
@@ -22,11 +25,42 @@ class TodoServiceTest {
 
     @Test
     @DisplayName("Agregar Todo")
-    public void guardarTodo(){
-        Todo todo = new Todo("Ir a cine",false,"1");
+    public void guardarTodo() {
+
+        //Arrange
+        Todo todo = new Todo("Ir a cine", false, "1");
+
+        //Act
         Todo todoRegistrado = todoRepository.save(todo);
+
+        //Assert
         assertNotNull(todoRegistrado);
 
     }
 
+    @Test
+    @DisplayName("Obtener por id")
+    public void obtenerPorId() {
+        Long idBuscado = 1L;
+        Optional<Todo> todoModelBuscado = todoRepository.findById(idBuscado);
+        assertThat(todoModelBuscado.get().getId()).isEqualTo(idBuscado);
+    }
+
+    @Test
+    @DisplayName("Listar todo")
+    public void listarTodo(){
+        List<Todo> todoList = (List<Todo>) todoRepository.findAll();
+        assertThat(todoList).size().isGreaterThan(0);
+    }
+
+    @Test
+    @DisplayName("Eliminar todo")
+    public void eliminarTodo(){
+        Long idTodo =1L;
+        boolean eliminarExistente = todoRepository.findById(idTodo).isPresent();
+        todoRepository.deleteById(idTodo);
+        boolean noExiste = todoRepository.findById(idTodo).isPresent();
+        assertTrue(eliminarExistente);
+        assertFalse(noExiste);
+    }
 }
