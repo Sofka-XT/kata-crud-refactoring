a0a3e6d601c436b22fa53b90c4b1ff2819c6739f
riveco0120 <richardsk.vellojin@gmail.com>
2022-05-04 16:46:51 -0500

    Servicios todo
    Se mappeo la entidad todo en los servicios

--- back/src/main/java/co/com/sofka/crud/service/TodoService.java
+++ back/src/main/java/co/com/sofka/crud/service/TodoService.java
@@ -1,25 +1,39 @@
 package co.com.sofka.crud.service;
 
+import co.com.sofka.crud.dto.TodoDto;
+import co.com.sofka.crud.mapper.MapperTodoDto;
 import co.com.sofka.crud.models.Todo;
 import co.com.sofka.crud.repository.TodoRepository;
 import co.com.sofka.crud.service.interfaces.ITodoService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 public class TodoService implements ITodoService {
 
     @Autowired
     private TodoRepository repository;
+    @Autowired
+    private ModelMapper modelMapper;
 
     @Override
-    public Iterable<Todo> list(){
-        return repository.findAll();
+    public Iterable<TodoDto> list(){
+        List<TodoDto>dtos = new ArrayList<>();
+        repository.findAll().forEach(todo->dtos.add(modelMapper.map(todo,TodoDto.class)));
+        return dtos;
     }
 
     @Override
-    public Todo save(Todo todo){
-        return repository.save(todo);
+    public TodoDto save(TodoDto dto){
+        Todo todoEntity =modelMapper.map(dto,Todo.class);
+        todoEntity = repository.save(todoEntity);
+        return modelMapper.map(todoEntity,TodoDto.class);
     }
 
     @Override
@@ -29,7 +43,11 @@ public class TodoService implements ITodoService {
 
     @Override
     public Todo get(Long id){
-         return repository.findById(id).orElseThrow();
+        Optional<Todo> optionalTodo = repository.findById(id);
+        if(optionalTodo.isEmpty()){
+            throw new NoSuchElementException("No existe una lista con ese id");
+        }
+        return repository.findById(id).orElseThrow();
     }
 
 }
