8eb215371d0c830fd0e3aeb50e5bb3cbbf9ec6a6
riveco0120 <richardsk.vellojin@gmail.com>
2022-05-05 16:10:01 -0500

    Agregando entidad mapper todolist
     se agrego la entidad para mappear la entidad todo list

--- back/src/main/java/co/com/sofka/crud/mapper/MapperTodoList.java
+++ back/src/main/java/co/com/sofka/crud/mapper/MapperTodoList.java
@@ -1,2 +1,33 @@
-package co.com.sofka.crud.mapper;public class MapperTodoList {
+package co.com.sofka.crud.mapper;
+
+import co.com.sofka.crud.dto.TodoListDto;
+import co.com.sofka.crud.mapper.intefezamapper.IMapperTodoList;
+import co.com.sofka.crud.models.TodoList;
+import org.mapstruct.Mapper;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Mapper(componentModel = "spring")
+public class MapperTodoList implements IMapperTodoList {
+
+    @Autowired
+    TodoList todoList1;
+
+    @Autowired
+    TodoListDto listDto;
+
+    @Autowired
+    private ModelMapper mapper;
+
+    @Override
+    public TodoListDto entitytodto(TodoList listtodo) {
+        listDto = mapper.map(listtodo, TodoListDto.class);
+        return listDto;
+    }
+
+    @Override
+    public TodoList dtoToEntity(TodoListDto dto) {
+        todoList1 = mapper.map(dto, TodoList.class);
+        return todoList1;
+    }
 }
