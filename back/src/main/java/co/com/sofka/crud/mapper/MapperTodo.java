2680783ddca7455d1572949174d4e692906a90f7
riveco0120 <richardsk.vellojin@gmail.com>
2022-05-05 15:50:51 -0500

    Se actualiza Mapper Todo
    Se actualizo la clase mapper todo para ser mas entendible el mapeo

--- back/src/main/java/co/com/sofka/crud/mapper/MapperTodo.java
+++ back/src/main/java/co/com/sofka/crud/mapper/MapperTodo.java
@@ -1,17 +1,35 @@
 package co.com.sofka.crud.mapper;
 
+import co.com.sofka.crud.dto.TodoDto;
+import co.com.sofka.crud.mapper.intefezamapper.IMapperTodo;
+import co.com.sofka.crud.models.Todo;
+import org.mapstruct.Mapper;
 import org.modelmapper.ModelMapper;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class Mapper {
+import org.springframework.beans.factory.annotation.Autowired;
 
 
-    @Bean
-    public ModelMapper modelMapper(){
 
-        return new ModelMapper();
+@Mapper(componentModel="spring")
+public class MapperTodo implements IMapperTodo {
 
+    @Autowired
+    Todo todo1;
+
+    @Autowired
+    TodoDto dto;
+
+    @Autowired
+    private ModelMapper mapper;
+
+    @Override
+    public TodoDto entitytodto(Todo todo) {
+    dto = mapper.map(todo,TodoDto.class);
+        return dto;
+    }
+
+    @Override
+    public Todo dtoToEntity(TodoDto dto) {
+        todo1 = mapper.map(dto,Todo.class);
+        return todo1;
     }
 }
