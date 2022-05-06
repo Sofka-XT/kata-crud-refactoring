2353a6d1ca6835e9d8023ac6e1040594c89b7d1b
riveco0120 <richardsk.vellojin@gmail.com>
2022-05-05 19:15:35 -0500

    Actualizando

--- back/src/main/java/co/com/sofka/crud/CrudApplication.java
+++ back/src/main/java/co/com/sofka/crud/CrudApplication.java
@@ -1,13 +1,19 @@
 package co.com.sofka.crud;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudApplication {
 
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(CrudApplication.class, args);
 	}
-
 }
--- back/src/main/java/co/com/sofka/crud/mapper/intefezamapper/IMapperTodo.java
+++
@@ -1,9 +0,0 @@
-package co.com.sofka.crud.mapper.intefezamapper;
-
-import co.com.sofka.crud.dto.TodoDto;
-import co.com.sofka.crud.models.Todo;
-
-public interface IMapperTodo {
-    public TodoDto entitytodto(Todo todo);
-    public Todo  dtoToEntity(TodoDto dto);
-}
